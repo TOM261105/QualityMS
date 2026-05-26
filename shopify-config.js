d9acc729cd466724956bdc4efbd87e9b0f8b2fb9
TOM261105 <tomas.gtz.vsco@gmail.com>
2026-05-26 17:41:39 -0600

    Create shopify-config.js

---
+++ shopify-config.js
@@ -0,0 +1,9 @@
+const SHOPIFY_CONFIG = {
+  useShopify: false,
+
+  // Cuando compren Shopify, aquí van los datos reales:
+  shopDomain: "tu-tienda.myshopify.com",
+  storefrontAccessToken: "TU_STOREFRONT_ACCESS_TOKEN",
+
+  apiVersion: "2026-01"
+};

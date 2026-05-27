c0614a7462d9707c66aece67854464a54b2bd731
TOM261105 <tomas.gtz.vsco@gmail.com>
2026-05-26 19:14:46 -0600

    Update shopify-config.js

--- shopify-config.js
+++ shopify-config.js
@@ -1,9 +1,7 @@
 const SHOPIFY_CONFIG = {
   useShopify: false,
 
-  // Cuando compren Shopify, aquí van los datos reales:
   shopDomain: "tu-tienda.myshopify.com",
   storefrontAccessToken: "TU_STOREFRONT_ACCESS_TOKEN",
-
   apiVersion: "2026-01"
 };
